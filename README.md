4f93a039b92bc585686a133a69a75e6cd886c508
ocadaruma <ocadaruma@gmail.com>
2021-03-30 03:21:20 +0900

    it works

---
+++ README.md
@@ -0,0 +1,8 @@
+# RuMikan
+
+[MikanOS](https://github.com/uchan-nos/mikanos) in Rust.
+
+## Prerequisites
+
+- LLD
+- rustc >= `1.53.0-nightly`
--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -8,39 +8,67 @@ extern crate uefi_services;
 use uefi::prelude::*;
 use core::fmt::Write;
 use uefi::proto::media::fs::SimpleFileSystem;
-use uefi::proto::media::file::{File, FileMode, FileType, FileAttribute, RegularFile};
-use uefi::table::boot::MemoryType;
+use uefi::proto::media::file::{File, FileMode, FileType, FileAttribute, RegularFile, FileInfo};
+use uefi::table::boot::{MemoryType, AllocateType};
 use core::cell::UnsafeCell;
 use uefi::proto::loaded_image::{LoadedImage, DevicePath};
 
 #[entry]
 fn efi_main(image_handle: uefi::Handle,
             system_table: SystemTable<Boot>) -> Status {
-    let mut buf = [0u8;4096 * 4];
+    uefi_services::init(&system_table)
+        .expect_success("Failed to initialize");
+
+    system_table.stdout().reset(false)
+        .expect_success("Failed to reset text output");
+
     let bt = system_table.boot_services();
-    let fs = get_image_fs(bt, image_handle)?
-        .expect("Failed to retrieve `SimpleFileSystem` on device");
+    let fs = get_image_fs(bt, image_handle)
+        .expect_success("Failed to retrieve `SimpleFileSystem` on device");
     let fs = unsafe { &mut *fs.get() };
-    match fs
+
+    let entry_addr = match fs
         .open_volume()
         .expect_success("Failed to open volume")
-        .open("\\memmap", FileMode::CreateReadWrite, FileAttribute::empty())
+        .open("\\rumikan-kernel", FileMode::Read, FileAttribute::empty())
         .expect_success("Failed to open file")
         .into_type()
         .expect_success("Failed to get regular file") {
-        FileType::Regular(file) => {
-            let (_k, desc_iter) = bt.memory_map(&mut buf)
-                .expect_success("Failed to get memory map");
-            let mut writer = FileWriter(file);
-            writer.write_str("index,type,type(name),physical_start,num_pages,attribute\n").unwrap();
-            for (i, desc) in desc_iter.enumerate() {
-                writer.write_fmt(
-                    format_args!("{},{},{},{},{}\n",
-                                 i, desc.ty.name(), desc.phys_start, desc.page_count, desc.att.bits())).unwrap();
-            }
+        FileType::Regular(mut file) => {
+            let mut buf = [0u8;1024];
+            let info = file.get_info::<FileInfo>(&mut buf)
+                .expect_success("Failed to get file info");
+
+            let addr = bt
+                .allocate_pages(AllocateType::Address(0x100000),
+                                MemoryType::LOADER_DATA,
+                                (info.file_size() as usize + 0xfff) / 0x1000)
+                .expect_success("Failed to allocate pages");
+
+            let buf = unsafe {
+                core::slice::from_raw_parts_mut(addr as *mut u8, info.file_size() as usize)
+            };
+            file.read(buf)
+                .expect_success("Failed to read kernel file");
+
+            let entry_addr = unsafe { *((addr + 24) as *const u64) };
+            Ok(entry_addr)
+        },
+        _ => {
+            Err(())
         }
-        _ => {}
-    }
+    }.expect("Failed to retrieve entry address");
+
+    system_table.stdout().write_fmt(
+        format_args!("entry_addr: 0x{:x}", entry_addr)).unwrap();
+    let entry_point: extern "C" fn() = unsafe { core::mem::transmute(entry_addr) };
+
+    let mut mmap_buf = [0u8;4096 * 4];
+    system_table.exit_boot_services(image_handle, &mut mmap_buf)
+        .expect_success("Failed to exit boot services");
+
+    entry_point();
+
     loop {}
 }
 
@@ -71,20 +99,24 @@ impl Display for MemoryType {
     }
 }
 
+/// Retrieves the `SimpleFileSystem` protocol associated with
+/// the device the given image was loaded from.
+///
+/// The code is taken from https://github.com/rust-osdev/uefi-rs/blob/724d1d64c6641f1af0735f049d967310665cf0b8/src/table/boot.rs#L571
 fn get_image_fs(bt: &BootServices, image_handle: Handle) -> uefi::Result<&UnsafeCell<SimpleFileSystem>> {
-    let loaded_image = bt.handle_protocol::<LoadedImage>(image_handle)?
-        .expect("Failed to retrieve `LoadedImage` protocol from handle");
+    let loaded_image = bt.handle_protocol::<LoadedImage>(image_handle)
+        .expect_success("Failed to retrieve `LoadedImage` protocol from handle");
     let loaded_image = unsafe { &*loaded_image.get() };
 
     let device_handle = loaded_image.device();
 
-    let device_path = bt.handle_protocol::<DevicePath>(device_handle)?
-        .expect("Failed to retrieve `DevicePath` protocol from image's device handle");
+    let device_path = bt.handle_protocol::<DevicePath>(device_handle)
+        .expect_success("Failed to retrieve `DevicePath` protocol from image's device handle");
 
     let device_path = unsafe { &mut *device_path.get() };
 
-    let device_handle = bt.locate_device_path::<SimpleFileSystem>(device_path)?
-        .expect("Failed to locate `SimpleFileSystem` protocol on device");
+    let device_handle = bt.locate_device_path::<SimpleFileSystem>(device_path)
+        .expect_success("Failed to locate `SimpleFileSystem` protocol on device");
 
     bt.handle_protocol::<SimpleFileSystem>(device_handle)
 }
