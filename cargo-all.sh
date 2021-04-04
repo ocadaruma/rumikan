09c6e32b3592cd1bf087cd335ebbbf9add432662
ocadaruma <ocadaruma@gmail.com>
2021-04-05 02:20:51 +0900

    run test

--- cargo-all.sh
+++ cargo-all.sh
@@ -6,6 +6,10 @@ script_dir="$(cd $(dirname $0) && pwd)"
 projects=(shared kernel-lib kernel bootloader)
 
 for project in ${projects[@]}; do
+  # currently tests are not executable on kernel and bootloader
+  if [[ "$1" == "test" && ("$project" == "kernel" || "$project" == "bootloader") ]]; then
+    continue
+  fi
   cd "$script_dir/$project"
   cargo $@
 done
